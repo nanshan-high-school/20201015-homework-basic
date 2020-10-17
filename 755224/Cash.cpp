62faab9acc633ee6d2884fee794cf1949d46295c
BrineYT <60344483+BrineYT@users.noreply.github.com>
2020-10-17 14:52:07 +0800

    Make the code even shorter

--- 755224/Cash.cpp
+++ 755224/Cash.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main() {
     int current[9] = {10000, 5000, 1000, 500, 100, 50, 10, 5, 1};
     int amount[9] = {0};
-    int counter = 0;
 
     float moneyf;
     cout << "How much money do you have: ";
@@ -16,21 +15,18 @@ int main() {
         cin >> moneyf;
     } while (10 * moneyf != money);
 
-    while (money != 0 && counter <= 8) {
+    int count = 0;
+    for (int counter=0; money != 0 && counter != 9; counter++) {
         while (money - current[counter] >= 0) {
             money -= current[counter];
             amount[counter]++;
         }
-        counter++;
-    }
-    
-    cout << "The simplest form to present " << moneyf << " is:\n";
-    int count = 0;
-    for (counter = 0; counter != 9; counter++) {
         if (amount[counter] != 0) {
             cout << current[counter] * 0.1 << " * " << amount[counter] << "\n";
         }
         count += amount[counter];
     }
+    
+    cout << "The simplest form to present " << moneyf << " is:\n";
     cout << "Total: " << count;
 }
