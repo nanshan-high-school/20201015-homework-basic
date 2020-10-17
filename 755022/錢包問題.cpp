c36c824c210f29b99ab01144b4c6f9f96522a993
JimmyNH3 <jimmy20060504@gmail.com>
2020-10-17 13:55:59 +0800

    1017錢包問題作業
    錢包問題

---
+++ 755022/錢包問題.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    float moneyF;
+
+    do {
+        cout << "輸入金額";
+        cin >> moneyF;
+    }
+    while (moneyF <= 0);
+
+    int moneyI = moneyF * 10, num = 0;
+
+    while(moneyI >= 10000) {
+        moneyI -= 10000;
+        num++;
+    }
+    while(moneyI >= 5000) {
+        moneyI -= 5000;
+        num++;
+    }
+    while(moneyI >= 1000) {
+        moneyI -= 1000;
+        num++;
+    }
+    while(moneyI >= 500) {
+        moneyI -= 500;
+        num++;
+    }
+    while(moneyI >= 100) {
+        moneyI -= 100;
+        num++;
+    }
+    while(moneyI >= 50) {
+        moneyI -= 50;
+        num++;
+    }
+    while(moneyI >= 10) {
+        moneyI -= 10;
+        num++;
+    }
+    while(moneyI >= 5) {
+        moneyI -= 5;
+        num++;
+    }
+    while(moneyI >= 1) {
+        moneyI -= 1;
+        num++;
+    }
+    cout << num;
+}
