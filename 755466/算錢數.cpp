36847317d80c7f66ca0a7ce3763567eee8db0f1b
Andy謝 <a755466@stdmail.nssh.ntpc.edu.tw>
2020-10-18 12:11:54 +0800

    29544 755466  謝奕宏
    29544 755466 謝奕宏

--- 755466/算錢數.cpp
+++ 755466/算錢數.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int main() {
     float money;
-    int total = 0, ans = 0;
+    int total = 0, ans = 0;  
 
-
-    cout << "ª÷ÃB¡G";
+    
+    cout << "é‡‘é¡ï¼š";
     cin >> money;
     ans = money * 10;
     if (ans < 0) {
-        cout << "ª÷ÃB¤£¥i¯à¤p©ó0³á!";
+        cout << "é‡‘é¡ä¸å¯èƒ½å°æ–¼0å–”!";
     }
 
     else {
@@ -27,12 +27,12 @@ int main() {
             total = total + ans / 1000;
             ans %= 1000;
         } while (ans > 1000);
-
+        
         do {
             total = total + ans / 500;
             ans %= 500;
         } while (ans > 500);
-
+            
         do {
             total = total + ans / 100;
             ans %= 100;
@@ -47,16 +47,16 @@ int main() {
             total = total + ans / 10;
             ans %= 10;
         } while (ans > 10);
-
+            
         do {
             total = total + ans / 5;
             ans %= 5;
         } while (ans > 5);
-
+        
         do {
             total = total + ans / 1;
             ans %= 1;
         } while (ans > 1);
-        cout << "»Ý­n" << total << "­Ó¯È¶r©Îµw¹ô";
-    }
-}
+        cout << "éœ€è¦" << total << "å€‹ç´™éˆ”æˆ–ç¡¬å¹£";
+    } 
+} 
