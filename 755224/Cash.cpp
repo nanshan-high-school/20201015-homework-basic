057b23ebb0d5f00ae8560e1680630f0c8d7f43c2
BrineYT <60344483+BrineYT@users.noreply.github.com>
2020-10-20 22:21:36 +0800

    Change the do-while part

--- 755224/Cash.cpp
+++ 755224/Cash.cpp
@@ -4,16 +4,20 @@ using namespace std;
 int main() {
     int current[9] = {10000, 5000, 1000, 500, 100, 50, 10, 5, 1};
     int amount[9] = {0};
-
     float moneyf;
-    cout << "How much money do you have(Minimum unit: 0.1): ";
-    cin >> moneyf;
-    int money = 10 * moneyf;
+    int money;
     
     do {
-        cout << "Confirm your money: ";
+        cout << "\nEnter your money: ";
         cin >> moneyf;
-    } while (10 * moneyf != money);
+        money = 10 * moneyf;
+        if (moneyf < 0) {
+            cout << "Money must be greater than 0\n";
+        }
+        if (10 * moneyf != money) {
+            cout << "The minimum unit is 0.1!\n";
+        }
+    } while (10 * moneyf != abs(money));
 
     int count = 0;
     cout << "The simplest form to present " << moneyf << " is:\n";
