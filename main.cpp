6d7e968927d17f107c65569a1929a77ec8369194
ChouYi <chouyi1010@gmail.com>
2020-10-21 20:01:26 +0800

    28947周誼1015HW

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main() {
+  float a;
+  int b = 0;
+  int c = a * 10;
+    
+  do {
+    cout << "金額";
+    cin >> a;
+  } while (a < 0);
+
+  c = a * 10;
+  b = b + int(c / 10000);
+  c = a * 10;
+  c = c % 10000;
+  b = b + int(c / 5000);
+  c = c % 5000;
+  b = b + int(c / 1000);
+  c = c % 1000;
+  b = b + int(c / 500);
+  c = c % 500;
+  b = b + int(c / 100);
+  c = c % 100;
+  b = b + int(c / 50);
+  c = c % 50;  
+  b = b + int(c / 10);
+  c = c % 10;
+  b = b + int(c / 5);
+  c = c % 5;
+  b = b + int(c / 1);
+  c = c % 1;
+  cout << b;
+} 
+
+
+    
