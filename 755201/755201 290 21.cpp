444fe47220e20540f7f69491a000d3bc2e724f26
Ericning <ningj6137@gmail.com>
2020-10-21 20:48:31 +0800

    29021 755201 寧家楨 錢包app

---
+++ 755201/755201 290 21.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float money;
+
+    do {
+        cout << "¿é¤Jª÷ÃB";
+        cin >> money;
+    }
+    while (money <= 0);
+
+    int money1 = money * 10, time = 0;
+
+    while(money1 >= 10000) {
+        money1 -= 10000;
+        time++;
+    }
+    while(money1 >= 5000) {
+        money1 -= 5000;
+        time++;
+    }
+    while(money1 >= 1000) {
+        money1 -= 1000;
+        time++;
+    }
+    while(money1 >= 500) {
+        money1 -= 500;
+        time++;
+    }
+    while(money1 >= 100) {
+        money1 -= 100;
+        time++;
+    }
+    while(money1 >= 50) {
+        money1 -= 50;
+        time++;
+    }
+    while(money1 >= 10) {
+        money1 -= 10;
+        time++;
+    }
+    while(money1 >= 5) {
+        money1 -= 5;
+        time++;
+    }
+    while(money1 >= 1) {
+        money1 -= 1;
+        time++;
+    }
+    cout << time;
+}
