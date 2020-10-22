bf2c6666c4eb4a089c3d78b6cf05bd400c0a4a9a
游城棣 <70698299+phoenix58-justin@users.noreply.github.com>
2020-10-22 18:42:08 +0800

    Update 29020 755200 游城棣 錢包問題.cpp
    排版更正

--- 755200/29020 755200 游城棣 錢包問題.cpp
+++ 755200/29020 755200 游城棣 錢包問題.cpp
@@ -8,34 +8,33 @@ int main() {
     cout << "金額:" ;
     cin >> moneydot;
     moneydot = moneydot * 10;
-    }while(moneydot != (int)moneydot);
+    } while (moneydot != (int)moneydot);
     for (money = moneydot; money > 10000; money -= 10000) {
         bill ++;
     }        
-    for(money = money; money > 5000; money -= 5000) {
+    for (money = money; money > 5000; money -= 5000) {
         bill ++;
     }        
-    for(money = money; money > 1000; money -= 1000) {
+    for (money = money; money > 1000; money -= 1000) {
         bill ++;
     }
-    for(money = money; money > 500; money -= 500) {
+    for (money = money; money > 500; money -= 500) {
         bill ++;
     }
-    for(money = money; money > 100; money -= 100) {
+    for (money = money; money > 100; money -= 100) {
         bill ++;
     }
-    for(money = money; money > 50; money -= 50) {
+    for (money = money; money > 50; money -= 50) {
         bill ++;
     }
-    for(money = money; money > 10; money -= 100) {
+    for (money = money; money > 10; money -= 100) {
         bill ++;
     }
-    for(money = money; money > 5; money -= 5) {
+    for (money = money; money > 5; money -= 5){
         bill ++;
     }
-    for(money = money; money > 1; money -= 1) {
+    for (money = money; money > 1; money -= 1){
         bill ++;
     }
     cout << "最少需要:" << bill << "枚銅板或紙鈔";
 }
- 
